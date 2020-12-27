182b6629e714ea57a65ad9e21a678fdbc421372b
rudytak <75335232+rudytak@users.noreply.github.com>
2020-12-27 19:16:00 +0100

    Added background animation

--- sketch.js
+++ sketch.js
@@ -1,13 +1,68 @@
 var canv;
+let backgroundPoints = [];
+const drag = 0.9999;
+forceMult = 1;
 
 function setup() {
-    canv = createCanvas(innerWidth - 17, 2 * innerHeight);
+    canv = createCanvas(innerWidth, innerHeight);
+    appropriateCanvResize();
+
+    frameRate(30);
+
+    addDots();
 }
 
 function draw() {
-    background(0);
+    background(0, 35);
+
+    var distTotal = 0;
+    for (let bp of backgroundPoints) {
+        distTotal += bp.update();
+        bp.draw();
+    }
+
+    if (forceMult == Infinity || forceMult < 0.005) {
+        addDots();
+    }
+
+    forceMult = pow(distTotal / (backgroundPoints.length * width), 2);
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, 2 * innerHeight);
+    appropriateCanvResize(); //Resize  of the canvas
+    addDots();
+}
+
+function appropriateCanvResize() {
+    if (canv.height > innerHeight) { // Add space for scroll bar
+        document.body.style.overflowY = "scroll";
+        resizeCanvas(innerWidth - 17, canv.height);
+    } else {
+        document.body.style.overflowY = "hidden"; // Get rid of scroll bar
+        resizeCanvas(innerWidth, innerHeight);
+    }
+}
+
+function addDots() {
+    backgroundPoints = [];
+
+    var a = random(-7, 7);
+    var b = random(-7, 7);
+
+    for (let i = 0; i < width * height / 2000; i++) { //Add the background dots
+        var ang = random(0, 2 * PI);
+
+        backgroundPoints.push(
+            new Dot(
+                createVector(
+                    width / 2 + width / 4 * cos(ang) * random(0.85, 1),
+                    height / 2 + width / 4 * sin(ang) * random(0.85, 1)
+                ),
+                createVector(
+                    sin(ang) * a,
+                    cos(ang) * b
+                )
+            )
+        );
+    }
 }
