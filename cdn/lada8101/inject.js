d02f6e34ac280d3f0f0e895dab21fa8259e4d73c
rudytak <ondra4sedlacek@gmail.com>
2023-09-17 22:17:14 +0200

    Update inject.js

--- cdn/lada8101/inject.js
+++ cdn/lada8101/inject.js
@@ -15,12 +15,16 @@ function setup() {
         }
         
         #wrap{
-        	z-index: 1000;
+        	z-index: 1000 !important;
           position: sticky !important;
         }
         
         jdiv{
         	z-index: 10 !important;
+        }
+
+        #defaultCanvas0{
+        	z-index: 1001 !important;
         }
     </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
