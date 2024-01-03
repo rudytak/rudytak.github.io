f2c17c2a0217db921152b9092aaab580662cc667
rudytak <ondra4sedlacek@gmail.com>
2024-01-03 18:53:26 +0100

    .

--- cdn/_TMF37_10/sketch.js
+++ cdn/_TMF37_10/sketch.js
@@ -1,5 +1,5 @@
-let W = 800;
-let H = 800;
+let W = 700;
+let H = 700;
 
 let _NumberController = Object.assign(Object.create(Object.getPrototypeOf(dat.controllers.NumberControllerBox)), dat.controllers.NumberControllerBox);
 dat.controllers.NumberControllerBox.prototype.updateDisplay = function updateDisplay() {
@@ -122,36 +122,130 @@ async function main() {
       gui.updateDisplay();
     });
 
-    let _m_0 = sim_defaults_folder.add(sim_defaults, "m_0").name("m_0 (readonly)").onFinishChange(() => {
+    let _m_0 = sim_defaults_folder.add(sim_defaults, "m_0").name("m_0 (readonly) [A*m^2]").onFinishChange(() => {
       sim_defaults_folder.updateDisplay();
     });
     _m_0.__precision = 8;
 
-    let _n = sim_defaults_folder.add(sim_defaults, "magnet_count").name("Magnet count - n [arb.]").step(1).setValue(sim_defaults.magnet_count);
+    let _n = sim_defaults_folder.add(sim_defaults, "magnet_count").name("Magnet count - n [arb.]").step(1).min(0).setValue(sim_defaults.magnet_count);
 
-    let _magnet_orientation = sim_defaults_folder.add(sim_defaults, "magnet_orientation").name("Magnet orientation").options(["vertical", "radial", "tangent"]);
+    let _magnet_orientation = sim_defaults_folder.add(sim_defaults, "magnet_orientation").options(["vertical", "radial", "tangent"]).name("Magnet orientation");
 
     let _r = sim_defaults_folder.add(sim_defaults, "r").name("Spinner radius - r [m]").step(1e-4);
     _r.__precision = 6;
 
+    let _alpha = sim_defaults_folder.add(sim_defaults, "α").name("Spinner const. drag coef. - α [rad*s^(-2)]").step(1e-4);
+    _alpha.__precision = 6;
+
+    let _beta = sim_defaults_folder.add(sim_defaults, "β").name("Spinner lin. drag coef. - β [s^(-1)]").step(1e-4);
+    _beta.__precision = 6;
+
+    let _gamma = sim_defaults_folder.add(sim_defaults, "γ").name("Spinner quad. drag coef. - γ [rad^(-1)]").step(1e-4);
+    _gamma.__precision = 6;
+
     sim_defaults_folder.updateDisplay();
   }
 
-  gui.add({ create_si: create_si }, "create_si").name("Create simulation instance")
-  gui.add({ start: start }, "start").name("Run simulation")
+  gui.add({
+    create_si: () => {
+      create_si();
+      afterSiCreation();
+    }
+  }, "create_si").name("Create simulation instance")
+
+  let sim_spinners;
+  function afterSiCreation() {
+    {
+      function create_spinners() {
+        sim_spinners = gui.addFolder("Spinners")
+        sim_spinners.open();
+
+        sim_spinners.add({
+          add_spinner: () => {
+            si.spinner_params.push([
+              v(0, 0, 0),
+              0,
+              0,
+              sim_defaults.magnet_count,
+              sim_defaults.r,
+              sim_defaults.I,
+              sim_defaults.m_0,
+              sim_defaults.magnet_orientation,
+              sim_defaults.α,
+              sim_defaults.β,
+              sim_defaults.γ,
+              false
+            ]);
+            update_spinners();
+          }
+        }, "add_spinner").name("Add a spinner")
+        sim_spinners.add({
+          remove_spinner: () => {
+            if (si.spinner_params.length > 1) {
+              si.spinner_params.splice(sim_params.exports.length - 1, 1)
+            }
+            update_spinners();
+          }
+        }, "remove_spinner").name("Remove a spinner")
+
+        for (let i = 0; i < si.spinner_params.length; i++) {
+          const ii = i;
+
+          let folder = sim_spinners.addFolder(`Spinner ${i}`)
+
+          let S_folder = folder.addFolder("S")
+          S_folder.add(si.spinner_params[ii][0], "x").name("x [m]").step(0.001);
+          S_folder.add(si.spinner_params[ii][0], "y").name("y [m]").step(0.001);
+
+          let temp;
+          temp = folder.add(si.spinner_params[ii], 1).name("φ_0 [rad]").step(0.001)
+          temp.__precision = 5;
+          temp = folder.add(si.spinner_params[ii], 2).name("ω_0 [rad/s]").step(0.001)
+          temp.__precision = 5;
+
+          temp = folder.add(si.spinner_params[ii], 3).name("n [arb.]").step(1).min(0).setValue(si.spinner_params[ii][3]);
+          temp.__precision = 0;
+          temp = folder.add(si.spinner_params[ii], 4).name("r [m]").step(1e-4);
+          temp.__precision = 6;
+          temp = folder.add(si.spinner_params[ii], 5).step(1e-7).name("I [kg*m^2]")
+          temp.__precision = 8;
+          temp = folder.add(si.spinner_params[ii], 6).name("m_0 [A*m^2]")
+          temp.__precision = 8;
+          folder.add(si.spinner_params[ii], 7).options(["vertical", "radial", "tangent"]).name("Magnet orientation")
+
+          temp = folder.add(si.spinner_params[ii], 8).name("α [rad*s^(-2)]").step(1e-4);
+          temp.__precision = 6;
+          temp = folder.add(si.spinner_params[ii], 9).name("β [s^(-1)]").step(1e-4);
+          temp.__precision = 6;
+          temp = folder.add(si.spinner_params[ii], 10).name("γ [rad^(-1)]").step(1e-4);
+          temp.__precision = 6;
+
+          temp = folder.add(si.spinner_params[ii], 11).name("Constant ω");
+        }
+      }
+
+      function update_spinners() {
+        gui.removeFolder(sim_spinners)
+        create_spinners();
+      }
+    }
+    create_spinners();
+
+    gui.add({ start: start }, "start").name("Run simulation")
+  }
+
   gui.show();
 }
 
 let instance_created = false;
-function create_si(){
+function create_si() {
   si = new sim_instance(
     sim_params,
     RK_matrix.RK4,
     sim_defaults
   );
 
-  si.add_spinner(v(0, 0, 0), Math.PI / 6, -9.86);
-  si.add_spinner(v(0, 0.08, 0), 0, 9.86, true);
+  si.add_spinner(v(0, 0, 0), 0, 0);
 
   instance_created = true;
 }
@@ -168,48 +262,88 @@ function setup() {
   background("black");
 }
 
+let finished = false;
+let save_added = false;
 let zoom = 1;
 let zoom_speed = 1.1;
-let c_x = W/2;
-let c_y = H/2;
+let c_x = W / 2;
+let c_y = H / 2;
 function draw() {
   background(0);
   if (!libs_loaded) return;
   if (!instance_created) return;
 
   push();
-  translate(c_x,c_y);
+  translate(c_x, c_y);
   scale(1000 * zoom);
+
+  let tot_frames = Math.ceil((si.sim_run_params.end_time - si.sim_run_params.start_time) / si.sim_run_params.dt);
 
   if (!started) {
     si.reset_spinners();
-    for(let s of si.spinners){
+    for (let s of si.spinners) {
       s.draw();
     }
 
     pop();
 
     fill("white")
-    text("Not started", 10, 20)
+    text(`Time: ${si.sim_run_params.start_time}`, 10, 20)
+    text(`Frame: ${0}/${tot_frames}`, 10, 40)
+    text("NOT STARTED!", 10, 60)
     return;
   }
 
-  let is_done = si.runWebFrame();
-  console.log(is_done)
+  if (!finished) {
+    finished = si.runWebFrame();
+  } else {
+    for (let s of si.spinners) {
+      s.draw();
+    }
+
+    if (!save_added) {
+      gui.add({
+        save: () => {
+          saveFile(
+            si.sim_run_params.out_path,
+            fs._cache[si.sim_run_params.out_path]
+          )
+        }
+      }, "save").name("Save output");
+    }
+    save_added = true;
+  }
 
   pop();
   fill("white")
   text(`Time: ${si.t}`, 10, 20)
-  text(`Frame: ${si.frame}`, 10, 40)
+  text(`Frame: ${si.frame}/${tot_frames}`, 10, 40)
 }
 
 function mouseDragged(event) {
-  if(event.which == 1 && event.target.id == "defaultCanvas0"){
+  if (event.which == 1 && event.target.id == "defaultCanvas0") {
     c_x += event.movementX;
     c_y += event.movementY;
   }
 }
 
 function mouseWheel(event) {
-  zoom *= Math.pow(zoom_speed, -Math.sign(event.delta))
+  if (event.target.id == "defaultCanvas0") {
+    zoom *= Math.pow(zoom_speed, -Math.sign(event.delta))
+  }
+}
+
+function saveFile(filename, data) {
+  const blob = new Blob([data], { type: 'text/csv' });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveBlob(blob, filename);
+  }
+  else {
+    const elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = filename;
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
+  }
 }
